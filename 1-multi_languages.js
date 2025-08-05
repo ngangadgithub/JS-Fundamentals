c20f5175d8c8a2bac9d2fdb7665d1970005c8cbb
Dennis Nganga <144046264+ngangadgithub@users.noreply.github.com>
2025-08-05 18:11:43 +0300

    Add files via upload

---
+++ 1-multi_languages.js
@@ -0,0 +1,12 @@
+const myVar=("C is fun");
+console.log("C is fun");
+//Expected output:"C is fun"
+
+const myVar1=("Python is cool");
+console.log("Python is fun");
+//Expected output:"Python is fun"
+
+
+const myVar2=("Java Script is amazing");
+console.log("Java script is amazing");
+//Expected output:"Java Script is amazing"
