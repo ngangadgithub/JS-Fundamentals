8cf0c8b841d3a14103f1281c0cc446b4ce033173
Dennis Nganga <144046264+ngangadgithub@users.noreply.github.com>
2025-08-14 18:58:49 +0300

    Add files via upload

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,16 +1,5 @@
-const process=require('node:process');
+const process =require('node:process');
 
-process.on('beforeExit', (code) =>{
-  console.log('Argument found:', code);
-  });
-
-  process.on('exit', (code) => {
-    console.log('Arguments found:', code);
-  });
-
-  console.log('No argument:');
-  
-  //Print 
-  //No argument
-  //Argument found
-//Arguments found
+process.on('exit', (code) => {
+    console.log('No argument: ${code}');
+});
