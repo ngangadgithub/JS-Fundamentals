3a406aef6ab6d8c2ffa94ca30235cd79e2baca1f
Dennis Nganga <144046264+ngangadgithub@users.noreply.github.com>
2025-08-15 12:13:32 +0300

    Add files via upload

---
+++ 4-concat.js
@@ -0,0 +1,5 @@
+let str1 = "C is cool";
+let str2 = "C is undefined";
+let str3= "Undefined is undefined";
+let result = str1.concat(str2,str3);
+console.log(result); // "C is cool" // "C is undefined" //"Undefined is undefined"
